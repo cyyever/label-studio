1609fc920cb73827d3f873fdf90531df007a1e27
Yuanyuan Chen <cyyever@outlook.com>
2025-12-20 21:35:44 +0800

    Skip async tests
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- label_studio/tests/sdk/test_export.py
+++ label_studio/tests/sdk/test_export.py
@@ -16,6 +16,8 @@ def test_project(django_live_url, business_client):
     return ls, project
 
 
+# TODO: support pytest-asyncio, otherwise this test will be skipped
+@pytest.mark.skip(reason='pytest-asyncio is not supported in this version of Label Studio')
 async def test_project_async(django_live_url, business_client):
     ls = AsyncLabelStudio(base_url=django_live_url, api_key=business_client.api_key)
     project = await ls.projects.create(
